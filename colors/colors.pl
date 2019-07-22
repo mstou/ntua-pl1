f7e011ca56a88312a3c66d9bc8c247c45d7c1ec3
Miltiadis Stouras <m.stouras@gmail.com>
2019-07-22 22:46:16 +0300

    Add a new predicate that takes window's start index, colors in window and the frequency map and moves start until the first color appears only once in the window. [colors/Prolog]

--- colors/colors.pl
+++ colors/colors.pl
@@ -11,3 +11,33 @@ read_line(Stream, L) :-
     atom_codes(Atom, Line),
     atomic_list_concat(Atoms, ' ', Atom),
     maplist(atom_number, Atoms, L).
+
+% findNewStart(Start,FreqMap,Colors,NewStart,NewFreqMap,DroppedColors)
+findNewStart(Start,FreqMap,[_],NewStart,NewFreqMap,DroppedColors) :-
+  NewStart = Start,
+  NewFreqMap = FreqMap,
+  DroppedColors = [].
+
+findNewStart(Start,FreqMap,[H1|L],NewStart,NewFreqMap,DroppedColors) :-
+  get_assoc(H1,FreqMap,HeadFrequency),
+  (
+    HeadFrequency > 1 ->
+      NewFreq is HeadFrequency - 1,
+      Start_ is Start + 1,
+      put_assoc(H1,FreqMap,NewFreq,FreqMap_),
+      findNewStart(Start_,FreqMap_,L,NewStart,NewFreqMap,RestDropped),
+      append([H1],RestDropped,DroppedColors)
+      ;
+
+      NewStart is Start,
+      NewFreqMap = FreqMap,
+      DroppedColors = []
+  ).
+
+  % findStart test:
+  % empty_assoc(X),
+  % put_assoc(1,X,4,X1),
+  % put_assoc(4,X1,2,X2),
+  % put_assoc(3,X2,2,X3),
+  % put_assoc(2,X3,1,X4),
+  % findNewStart(0,X4,[1,1,1,4,3,1,3,2,4],NewStart,NewFreqMap,DroppedColors).
