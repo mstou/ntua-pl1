97933e30927a584a5293ed53252cc00eb92d85f9
Miltiadis Stouras <m.stouras@gmail.com>
2019-07-22 15:18:23 +0300

    Add 2 predicates to read the input. [colors/Prolog]

---
+++ colors/colors.pl
@@ -0,0 +1,13 @@
+% Predicates that read the input
+% https://courses.softlab.ntua.gr/pl1/2019a/Exercises/read_colors_SWI.pl
+
+read_input(File, N, K, C) :-
+    open(File, read, Stream),
+    read_line(Stream, [N, K]),
+    read_line(Stream, C).
+
+read_line(Stream, L) :-
+    read_line_to_codes(Stream, Line),
+    atom_codes(Atom, Line),
+    atomic_list_concat(Atoms, ' ', Atom),
+    maplist(atom_number, Atoms, L).
