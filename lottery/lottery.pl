c5aa6c46010e11813065186b36c8c861ba1d537b
Miltiadis Stouras <m.stouras@gmail.com>
2019-07-25 00:21:35 +0300

    Implement insertions on tries. [lottery/Prolog]

--- lottery/lottery.pl
+++ lottery/lottery.pl
@@ -30,3 +30,39 @@ read_tickets(Stream, Acc, N, Tickets) :-
     read_ticket(Stream, T),
     TicketsLeft is N-1,
     read_tickets(Stream, [T|Acc], TicketsLeft, Tickets).
+
+
+% datatype trie = empty | trie(node, children, tickets)
+%
+% children is an association list with key the children's node
+% and value a tuple with ticketsHanging from the children and the trie
+% rooted at the children.
+
+% insert(T,L,Tfinal)
+insert(T1,[],T2) :- T2 = T1.
+
+insert(empty, [X], T2) :-
+  empty_assoc(Y),
+  T2 = trie(X,Y,1).
+
+insert(empty,[X1,X2|L],T) :-
+  insert(empty,[X2|L],Child),
+  empty_assoc(Z),
+  put_assoc(X2,Z,(1,Child),Map),
+  T = trie(X1,Map,1).
+
+insert(trie(X,Map,N), [H|L], T2) :-
+  (
+    get_assoc(H,Map,Val) ->
+      (Tickets,ChildTrie) = Val,
+      insert(ChildTrie,L,NewChild),
+      NewTickets is Tickets + 1,
+      put_assoc(H,Map,(NewTickets,NewChild),NewMap)
+
+      ;
+
+      insert(empty,[H|L],NewChild),
+      put_assoc(H,Map,(1,NewChild),NewMap)
+  ),
+  NewN is N+1,
+  T2 = trie(X,NewMap,NewN).
