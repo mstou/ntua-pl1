12e72e91797be78806ea8157a224d8eb5f21b46a
Miltiadis Stouras <m.stouras@gmail.com>
2019-07-27 19:59:27 +0300

    Add a predicate that finds a node's neighbors. [ztalloc/Prolog]

--- ztalloc/ztalloc.pl
+++ ztalloc/ztalloc.pl
@@ -20,3 +20,40 @@ read_querries(Stream,N,Q,Acc) :-
     read_line(Stream, L),
     QuerriesLeft is N-1,
     read_querries(Stream,QuerriesLeft,Q,[L|Acc]).
+
+isValid(X) :-
+  X >= 0,
+  X =< 999999.
+
+isNodeValid(L,R) :-
+  isValid(L),
+  isValid(R).
+
+nodeNotVisited(L,R,Parent) :-
+  isNodeValid(L,R),
+  \+ get_assoc((L,R),Parent,_).
+
+% [L,R] is the current node
+% Parent is the association list we use to keep track of
+% visited nodes and their parents
+findNeighbors(L,R,Parent,Neighbors,NewParent) :-
+  L1 is 3*L+1,
+  R1 is 3*R+1,
+  (
+    nodeNotVisited(L1,R1,Parent) ->
+      Res1 = [(L1,R1)],
+      put_assoc((L1,R1),Parent,("t",L,R),Parent_)
+      ;
+      Res1 = [],
+      Parent_ = Parent
+  ),
+  L2 is L div 2,
+  R2 is R div 2,
+  (
+    nodeNotVisited(L2,R2,Parent_) ->
+      Neighbors = [(L2,R2)|Res1],
+      put_assoc((L2,R2),Parent_,("h",L,R),NewParent)
+      ;
+      Neighbors = Res1,
+      NewParent = Parent_
+  ).
